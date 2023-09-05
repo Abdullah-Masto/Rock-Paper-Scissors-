346bca59f39b05075ed7c074346c9637bddb9aa1
Abdullah-Masto <abdullahmasto77@gmail.com>
2023-09-05 15:34:27 +0300

    add UI to the project

--- script.js
+++ script.js
@@ -13,31 +13,75 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return `you tie! ${playerSelection} ties ${computerSelection}`;
+    return `even`;
   } else if (
     (playerSelection == "Rock" && computerSelection == "Scissors") ||
     (playerSelection == "Scissors" && computerSelection == "Paper") ||
     (playerSelection == "Paper" && computerSelection == "Rock")
   ) {
-    return `you Won! ${playerSelection} beats ${computerSelection}`;
+    return `you`;
   } else if (
     (computerSelection == "Rock" && playerSelection == "Scissors") ||
     (computerSelection == "Scissors" && playerSelection == "Paper") ||
     (computerSelection == "Paper" && playerSelection == "Rock")
   ) {
-    return `you Lose! ${computerSelection} beats ${playerSelection}`;
+    return `computer`;
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("enter your choice:").toLowerCase();
-    playerSelection = playerSelection
-      .substring(0, 1)
-      .toUpperCase()
-      .concat(playerSelection.substring(1));
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
+let again = document.createElement("button");
+again.textContent = "play again";
+again.classList.add("again");
+again.style.display = "none";
+document.body.appendChild(again);
+let you = document.querySelector(".you.points");
+let computer = document.querySelector(".computer.points");
+let state = document.querySelector(".state");
+state.style.display = "none";
+let tools = document.querySelectorAll(".tools>button");
+tools.forEach((tool) =>
+  tool.addEventListener("click", () =>
+    defineState(playRound(tool.id, getComputerChoice()))
+  )
+);
+
+function playAgain() {
+  you.textContent = 0;
+  computer.textContent = 0;
+  tools.forEach((tool) => (tool.disabled = true));
+  state.style.display = "block";
+  again.style.display = "block";
+}
+
+function defineState(winner) {
+  if (winner === "you") {
+    let youNum = Number(you.textContent);
+    youNum++;
+    if (youNum === 5) {
+      state.textContent = "You won!";
+      playAgain();
+    } else {
+      you.textContent = youNum;
+    }
+    return;
+  }
+  if (winner === "computer") {
+    let computerNum = Number(computer.textContent);
+    computerNum++;
+    if (computerNum === 5) {
+      state.textContent = "Computer won!";
+      playAgain();
+    } else {
+      computer.textContent = computerNum;
+    }
+    return;
   }
 }
-game();
+
+again.addEventListener("click", () => {
+  tools.forEach((tool) => {
+    tool.disabled = false;
+    again.style.display = "none";
+    state.style.display = "none";
+  });
+});
